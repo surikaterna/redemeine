9caf92bf69327aabf1fe9118525f099e02f2a7a4
spralle <sprawl@onlajn.nu>
2023-04-04 18:26:03 +0200

    small changes

--- test/createAggregate.test.ts
+++ test/createAggregate.test.ts
@@ -12,115 +12,115 @@ import { Command, Commands } from '../src/createCommand';
 // };
 
 describe('createAggregate', () => {
-    // test('returns object', () => {
-    //   const aggregate = createAggregate({
-    //     name: 'order',
-    //     initialState,
-    //     events: {
-    //       cancel: (state, cmd: { remark: string }) => {}
-    //     }
-    //   });
-    //   expect(aggregate).not.toBeNull();
-    // });
-    // /*
-    //       command -> process-> events -> apply -> state
-    //   */
+  // test('returns object', () => {
+  //   const aggregate = createAggregate({
+  //     name: 'order',
+  //     initialState,
+  //     events: {
+  //       cancel: (state, cmd: { remark: string }) => {}
+  //     }
+  //   });
+  //   expect(aggregate).not.toBeNull();
+  // });
+  // /*
+  //       command -> process-> events -> apply -> state
+  //   */
 
-    // interface AggDef<State = any> {
-    //   name: string;
-    //   initialState: State | (() => State);
-    //   // combo: [string, string, Function];
+  // interface AggDef<State = any> {
+  //   name: string;
+  //   initialState: State | (() => State);
+  //   // combo: [string, string, Function];
 
-    //   events: AggregateProjectors<State>;
-    // }
+  //   events: AggregateProjectors<State>;
+  // }
 
-    test('called command', () => {
-        interface TestState {
-            age: Number;
-        }
-        const o: AggregateDefinition<TestState> = {
-            name: 'test',
-            initialState: { age: 12 },
-            //   combo: ["cancel", "cancelled", () => {}],
-            commands: {
-                cancel: (state, event) => { },
-                cancelPermanently: (state, event) => { }
-            }
+  test('called command', () => {
+    interface TestState {
+      age: Number;
+    }
+    const o: AggregateDefinition<TestState> = {
+      name: 'test',
+      initialState: { age: 12 },
+      //   combo: ["cancel", "cancelled", () => {}],
+      commands: {
+        cancel: (state, event) => {},
+        cancelPermanently: (state, event) => {}
+      }
+    };
+
+    interface Command2<S = any, P extends any = any> {
+      type: string;
+      payload: P;
+    }
+
+    type Commands2<T extends keyof any = string, P extends any = any> = Record<T, () => Command2<any, P>>;
+
+    let cmds = {
+      test: () => {
+        return {
+          type: 'test',
+          payload: {}
         };
-
-        interface Command2<S = any, P extends any = any> {
-            type: string;
-            payload: P;
-        }
-
-        type Commands2<T extends keyof any = string, P extends any = any> = Record<T, () => Command2<any, P>>;
-
-        let cmds = {
-            test: () => {
-                return {
-                    type: 'test',
-                    payload: {}
-                };
-            },
-            test2: () => {
-                return {
-                    type: 'test2',
-                    payload: {}
-                };
-            }
+      },
+      test2: () => {
+        return {
+          type: 'test2',
+          payload: {}
         };
+      }
+    };
 
-        type ExtractType<TT extends Commands2> = {
-            [K in keyof TT]: TT[K];
-        };
+    type ExtractType<TT extends Commands2> = {
+      [K in keyof TT]: TT[K];
+    };
 
-        type TDescriptor<C extends Commands2<any>> = {
-            commands: C;
-            events?: null;
-        };
+    type TDescriptor<C extends Commands2<any>> = {
+      commands: C;
+      events?: null;
+    };
 
-        type TDecorator<TT extends Record<string, (...args: any) => any>> = {
-            [K in keyof TT]: (string) => ReturnType<TT[K]>;
-        };
+    type TDecorator<TT extends Record<string, (...args: any) => any>> = {
+      [K in keyof TT]: (string) => ReturnType<TT[K]>;
+    };
 
-        function tt<TT extends Commands2<any>>(cmds: TDescriptor<TT>): TDecorator<TT> {
-            const res = {};
-            Object.keys(cmds.commands).forEach((c) => (res[c] = (a: string) => cmds.commands[c]));
-            return res as TDecorator<TT>;
-        }
-        /*
+    function tt<TT extends Commands2<any>>(cmds: TDescriptor<TT>): TDecorator<TT> {
+      const res = {};
+      Object.keys(cmds.commands).forEach((c) => (res[c] = (a: string) => cmds.commands[c]));
+      return res as TDecorator<TT>;
+    }
+    /*
                   command -> process-> events -> apply -> state
         */
-        let cc = {
-            initialState: { value: 0 },
-            commands: {
-                inc: () => {
-                    return;
-                }, dec: () => { }
-            },
-            process: {
-                inc: (state: any, a: string, b: number) => { return { a, b } }
-            },
-            events: {
-                cancelled: ()=>{},
-                pCancelled: {
-                    process: ()=>{}, 
-                    project: ()=>{}
-                }
-            }
-        };
+    let cc = {
+      type: 'counter',
+      initialState: { value: 0 },
+      commands: {
+        inc: () => {
+          return;
+        },
+        dec: () => {}
+      },
+      process: {
+        inc: (state: any, a: string, b: number) => {
+          return { a, b };
+        }
+      },
+      events: {
+        cancelled: (remark: string) => {},
+        closed: {
+          close: null,
+          project: ()=>{}
+        }
+      }
+    };
 
-        tt({
-            commands: cmds,
-            events: {
-                cancelled: {
-                    cancel: {
+    //commands, processors, events, projectors
 
-                    }
-                }
-            }).test2('heelo');
+    tt({
+      commands: cmds
+    }).test2('heelo');
 
-        /*    let cp: CommandProcessors<TestState, { test: (state: TestState, cmd: any) => Record<T, Command<{}>> }> = {
+    /*    let cp: CommandProcessors<TestState, { test: (state: TestState, cmd: any) => Record<T, Command<{}>> }> = {
           test: (state: TestState, cmd: any) => {
             return {
               type: 'test',
@@ -129,23 +129,23 @@ describe('createAggregate', () => {
           }
         };
         */
-        //cp.test({ age: 12 }, { type: 'test', payload: {} });
-        // cp.test({ age: 12 }, );
+    //cp.test({ age: 12 }, { type: 'test', payload: {} });
+    // cp.test({ age: 12 }, );
 
-        //createAggregate(o).commands.cancelled2({ age: 12 }, null);
+    //createAggregate(o).commands.cancelled2({ age: 12 }, null);
 
-        //createCommand('order.cancel', ()=>{});
+    //createCommand('order.cancel', ()=>{});
 
-        // const aggregate = createAggregate({
-        //   name: "order",
-        //   initialState: initialState,
-        //   events: {
-        //     cancel: (state, cmd: { remark: string }) => {
-        //       done();
-        //     },
-        //   },
-        // });
-        // expect(aggregate.cancel).not.toBeNull();
-        // aggregate.cancel();
-    });
+    // const aggregate = createAggregate({
+    //   name: "order",
+    //   initialState: initialState,
+    //   events: {
+    //     cancel: (state, cmd: { remark: string }) => {
+    //       done();
+    //     },
+    //   },
+    // });
+    // expect(aggregate.cancel).not.toBeNull();
+    // aggregate.cancel();
+  });
 });
