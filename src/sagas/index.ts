781430cf473dd229e4298d7c7188aa91b15a1625
spralle <sprawl@onlajn.nu>
2026-03-31 07:19:05 +0200

    feat(sagas): add pending intent projection query model

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -2,4 +2,5 @@ export * from './events';
 export * from './createSaga';
 export * from './SagaRegistry';
 export * from './SagaEventStore';
+export * from './PendingIntentProjection';
 export * from './replayExecution';
