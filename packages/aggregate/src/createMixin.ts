59be85cb4fba5493edf8b65352fa1b8c51f70c19
spralle <sprawl@onlajn.nu>
2026-04-02 09:24:43 +0200

    refactor(aggregate): move aggregate-only typing surface out of kernel

--- packages/aggregate/src/createMixin.ts
+++ packages/aggregate/src/createMixin.ts
@@ -1,6 +1,7 @@
-import { Event, EventEmitterFactory, EventType, CommandType, SelectorsMap, MapCommandsToPayloads, CommandContext, CommandIntents } from '@redemeine/kernel';
+import { Event, EventType, CommandType, SelectorsMap, CommandContext, CommandIntents } from '@redemeine/kernel';
 import { RedemeineComponent, RedemeineCommandDefinition, RedemeineEventDefinition, NormalizeEventDefinitions, GenericCommandFactory, GenericCommandMap, createComponentBehaviorState, bindFluentMethods } from './redemeineComponent';
 import type { EntityPackage } from './createEntity';
+import type { EventEmitterFactory, MapCommandsToPayloads } from './types/aggregateTyping';
 import {
   MapEntityCommands,
   EntityListOptions,
