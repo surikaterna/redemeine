a6e94ff65e8319001df9438b902d75fc09fe73c9
Andreas Karlsson <spralle@gmail.com>
2026-04-09 20:43:40 +0200

    feat(redemeine-ato.3): scaffold v3 router and worker package boundaries

---
+++ packages/projection-worker-core/src/index.ts
@@ -0,0 +1,10 @@
+export type {
+  ProjectionDefinitionLike,
+  ProjectionRouteDecision,
+  ProjectionRouteTarget,
+  ProjectionRouterEnvelope,
+  ProjectionWorkerAck,
+  ProjectionWorkerMessage,
+  ProjectionWorkerPushContract,
+  ProjectionWorkerTransportMetadata
+} from './contracts';
